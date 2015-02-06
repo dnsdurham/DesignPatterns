ac667ad8b864244b747225e53cd9e0174ae1baac
Doug Durham <ddurham@nebraskaglobal.com>
2015-02-06 10:45:04 -0600

    Added basic Singleton project and test project

---
+++ DesignPatterns.Creational.Singleton/Singleton/Interfaces.cs
@@ -0,0 +1,8 @@
+﻿
+namespace DesignPatterns.Creational.Singleton
+{
+    public interface ISampleSingleton
+    {
+        int MyCounter { get; set; }
+    }
+}
---
+++ DesignPatterns.Creational.Singleton/Singleton/SampleSingleton.cs
@@ -0,0 +1,30 @@
+﻿
+namespace DesignPatterns.Creational.Singleton
+{
+    public class SampleSingleton :ISampleSingleton
+    {
+        // static instance of the singleton
+        //private static SampleSingleton _instance = null; // not thread-safe
+        private static SampleSingleton _instance = new SampleSingleton(); // thread-safe
+        
+        // private constructor
+        private SampleSingleton()
+        {
+        }
+
+        // public accessor to get the instance
+        public static SampleSingleton GetInstance
+        {
+            get
+            {
+                //if (_instance == null) // not thread-safe
+                //{
+                //    _instance = new SampleSingleton();
+                //}
+                return _instance;
+            }
+        }
+
+        public int MyCounter { get; set; }
+    }
+}
