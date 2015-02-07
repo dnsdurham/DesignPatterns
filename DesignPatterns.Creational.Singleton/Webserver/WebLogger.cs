d995af9f59d00665e4ea3676489e895da10d5ca9
Doug Durham <ddurham@nebraskaglobal.com>
2015-02-07 10:06:50 -0600

    Additional classed for Webserver2

---
+++ DesignPatterns.Creational.Singleton/Webserver/WebLogger.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+namespace DesignPatterns.Creational.Webserver
+{
+    /// <summary>
+    /// This class implements a webservers logfile writing as a class Singleton
+    /// </summary>
+    public class WebLogger
+    {
+        private static WebLogger _instance = new WebLogger();
+        static readonly object _locker = new object(); // used for synchronization
+        private WebLogger()
+        {
+        }
+
+        public static WebLogger GetInstance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        public void WriteEntry(string logLine)
+        {
+            lock (_locker) // need to synchronize thread access to this method
+            {
+                using (StreamWriter logFile = new StreamWriter(@"webserverlog.txt", true))
+                {
+                    logFile.WriteLine(logLine);
+                }
+            }
+        }
+    }
+}
---
+++ DesignPatterns.Creational.Singleton/Webserver2/Interfaces.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Webserver2
+{
+    public interface IWebserver2Factory
+    {
+        IRequestHandler2 GetRequestHandler(IWebserver2Factory factory);
+
+        IWebLogger2 GetWebLogger();
+    }
+
+    public interface IRequestHandler2
+    {
+        string ProcessRequest(string request);
+    }
+
+    public interface IWebLogger2
+    {
+        void WriteEntry(string logLine);
+    }
+}
