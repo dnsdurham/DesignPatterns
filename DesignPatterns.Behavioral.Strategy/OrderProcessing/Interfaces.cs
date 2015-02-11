b2a123e99f9e4f0880c9213ca6fac4328ca89939
Doug Durham <ddurham@nebraskaglobal.com>
2015-02-11 09:39:01 -0600

    polishing code and adding unit tests

--- DesignPatterns.Behavioral.Strategy/OrderProcessing/Interfaces.cs
+++ DesignPatterns.Behavioral.Strategy/OrderProcessing/Interfaces.cs
@@ -1,6 +1,14 @@
-﻿namespace DesignPatterns.Behavioral.Strategy.OrderProcessing
+﻿
+namespace DesignPatterns.Behavioral.Strategy.OrderProcessing
 {
-    class Interfaces
+    public interface IOrder
     {
+        // this is returning the total amount of the order for demo purposes
+        decimal Process(ICalculateTax taxCalculator);
+    }
+
+    public interface ICalculateTax
+    {
+        decimal TaxAmount(long itemSold, decimal price, bool isFood);
     }
 }
---
+++ DesignPatterns.Behavioral.Tests/TaxCalculatorTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DesignPatterns.Behavioral.Strategy.OrderProcessing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignPatterns.Behavioral.Tests
+{
+    [TestClass]
+    public class TaxCalculatorTests
+    {
+        [TestMethod]
+        public void UsTax_TaxAmount()
+        {
+            ICalculateTax taxCalculator = new UsTax();
+   
+            Assert.AreEqual(0.70m, taxCalculator.TaxAmount(2, 5.00m, false));
+            Assert.AreEqual(27.96m, taxCalculator.TaxAmount(4, 99.87m, false));
+
+        }
+
+        [TestMethod]
+        public void CanadianTax_TaxAmount()
+        {
+            ICalculateTax taxCalculator = new CanadianTax();
+
+            Assert.AreEqual(1.50m, taxCalculator.TaxAmount(2, 5.00m, false));
+            Assert.AreEqual(98.87m, taxCalculator.TaxAmount(4, 99.87m, false));
+
+        }
+    }
+}
